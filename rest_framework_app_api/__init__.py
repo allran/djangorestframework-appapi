ff743465c6462a5ba0e1ff7a2e70e1c317abc48a
allran <284051099@qq.com>
2019-10-14 10:33:04 +0800

    添加版本

---
+++ rest_framework_app_api/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'djangorestframework-appapi'
+__version__ = '0.0.1'
+__author__ = ''
+__license__ = 'BSD'
+__copyright__ = ''
+
+# Version synonym
+VERSION = __version__
